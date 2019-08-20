38d96088881bfad35f5821de73d27d0f8fadec91
leeluolee <87399126@163.com>
2019-08-20 21:09:24 +0800

    feat: add progress for connecting to server

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-
 const localtunnel = require('localtunnel');
 
 module.exports = {
   // define props
   configSchema: {
-
     subdomain: {
       type: 'string',
       description:
-        'A string value requesting a specific subdomain on the proxy server',
+        'A string value requesting a specific subdomain on the proxy server'
     },
     localhost: {
       type: 'string',
-      description: 'Proxy to this hostname instead of localhost',
+      description: 'Proxy to this hostname instead of localhost'
     },
     host: {
       type: 'string',
-      description: 'use your own localtunnel server, defualt is "https://localtunnel.me"',
-    },
+      description:
+        'use your own localtunnel server, defualt is "https://localtunnel.me"'
+    }
   },
   hooks: {
-    async onCreate({ io, config, events, logger }) {
+    async onCreate({ config, events, logger }) {
       let tunnel;
 
-      events.on('ready', ()=>{
+      events.on('ready', () => {
+        const stopSpin = logger.spin('connecting to localtunnel...');
         const port = config.get('$.port');
-        tunnel = localtunnel(port, {
-          host: config.get('host'),
-          local_host: config.get('localhost'),
-          subdomain: config.get('subdomain'),
-        }, (err, tunnel)=>{
-          if(err) logger.error(`localtunnel setup failed, beacuse {err.stack || err.message}`)
-          logger.notify(`url is available on ${tunnel.url}`)
-        })
-        tunnel.on('error', (err)=>{
-          logger.error(err.message)
-        })
-      })
+        tunnel = localtunnel(
+          port,
+          {
+            host: config.get('host'),
+            local_host: config.get('localhost'),
+            subdomain: config.get('subdomain')
+          },
+          (err, tunnel) => {
+            stopSpin();
+            if (err)
+              logger.error(
+                `localtunnel setup failed, beacuse {err.stack || err.message}`
+              );
+            logger.notify(`url is available on ${tunnel.url}`);
+          }
+        );
+        tunnel.on('error', err => {
+          logger.error(err.message);
+        });
+      });
       // onCreate
       return () => {
-        if(tunnel) tunnel.close();
-      }
+        if (tunnel) tunnel.close();
+      };
     }
   }
 };
